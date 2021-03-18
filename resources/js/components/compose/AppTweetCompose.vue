a5567e8051d3a8a178c347202626c453ffd1f50a
Alex Malikov <alex.malikov94@gmail.com>
2021-03-18 10:35:16 +0500

    Automatically adjusting the textarea height

--- resources/js/components/compose/AppTweetCompose.vue
+++ resources/js/components/compose/AppTweetCompose.vue
@@ -4,8 +4,7 @@
           <img :src="$user.avatar" class="w-12 rounded-full">
      </div>
      <div class="flex-grow">
-         <textarea class="bg-gray-900 w-full outline-none text-gray-300 text-lg resize-none mb-2" 
-         placeholder="What's happening?" autofocus v-model="form.body"></textarea>
+      <app-tweet-compose-textarea v-model="form.body" />
        <div class="flex justify-between">
           <div>
              
@@ -34,6 +33,7 @@ export default {
     methods: {
         async submit() {
            await axios.post('/api/tweets', this.form)
+           this.form.body = ''
         }
     }
 }
