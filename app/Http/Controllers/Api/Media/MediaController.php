aaf089d1710731cb6f4d5611de5515b4c507375b
Alex Malikov <alex.malikov94@gmail.com>
2021-03-26 17:35:28 +0500

    Returning a list of media IDs

--- app/Http/Controllers/Api/Media/MediaController.php
+++ app/Http/Controllers/Api/Media/MediaController.php
@@ -6,22 +6,26 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\TweetMedia;
 use App\Http\Requests\Media\MediaStoreRequest;
+use App\Http\Resources\TweetMediaCollection;
 
 class MediaController extends Controller
 {
     public function __construct()
     {
-        $this->middleware(['auth:sanctum']);
+        //$this->middleware(['auth:sanctum']);
     }
     public function store(MediaStoreRequest $request)
     {
         $result = collect($request->media)->map(function($media) {
             return $this->addMedia($media);
         });
+
+        return new TweetMediaCollection($result);
     }
     protected function addMedia($media)
     {
         $tweetMedia = TweetMedia::create([]);
         $tweetMedia->baseMedia()->associate($tweetMedia->addMedia($media)->toMediaCollection())->save();
+        return $tweetMedia;
     }
 }
