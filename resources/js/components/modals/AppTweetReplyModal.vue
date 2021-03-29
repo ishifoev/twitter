909482b7a06653ca1c9f9415f74bf91d8ec0322a
Alex Malikov <alex.malikov94@gmail.com>
2021-03-29 17:48:10 +0500

    Reply modal

---
+++ resources/js/components/modals/AppTweetReplyModal.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+       <component class="mb-4" v-if="tweet" :is="`app-tweet-variant-${tweet.type}`" :tweet="tweet" />
+       <app-tweet-reply-compose v-if="tweet" :tweet="tweet" />
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        tweet: {
+            required: true,
+            type: Object
+        }
+    }
+}
+</script>
